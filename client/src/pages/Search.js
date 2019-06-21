f36e2514f14c60bdb594fee66b5a75102da617d3
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-06-21 18:18:30 -0400

    backup

---
+++ client/src/pages/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+
+class Search extends Component {
+    state = {
+        search: "",
+        results: [],
+        error: ""
+    };
+    render() {
+        return (
+
+        )
+    }
+}
