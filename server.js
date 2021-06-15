614726f1f739f8e56a43e2930ffb931a493ec1c5
Weibin Ma <weibinma@weibins-mbp.lan>
2021-06-15 18:07:22 -0400

    change mongodb url

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const routes = require("./routes");
 const mongoose = require("mongoose");
@@ -16,9 +16,19 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist2";
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist2";
+// mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+const CONNECTION_URL = 'mongodb+srv://davidma:davidma123@cluster0.qdcbx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  }))
+  .catch((error) => {console.log(error.message)})
+
+  mongoose.set('useFindAndModify', false);
+
+// app.listen(PORT, () => {
+//   console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
